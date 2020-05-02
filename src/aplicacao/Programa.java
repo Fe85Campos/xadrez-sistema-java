c8e7545d6c633a69d2cb15e9ce15813e833fc912
Fernando Bomfim <fe85campos@gmail.com>
2020-05-02 11:57:17 -0300

    Primeira classe: Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import tabuleiro.Posicao;
+
 public class Programa {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Olá mundo!");
+		Posicao pos = new Posicao(4, 8);
+		
+		System.out.println(pos);
 	}
 
 }
